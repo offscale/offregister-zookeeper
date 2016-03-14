67c6cfdd2010bcde733f12b2e3beca0d9809dede
Samuel Marks <samuelmarks@gmail.com>
2016-03-14 15:00:15 +1100

    Init

---
+++ README.md
@@ -0,0 +1,14 @@
+offregister_zookeeper
+===============
+
+[Apache ZooKeeper](https://zookeeper.apache.org) is a centralized service for maintaining configuration information, naming, providing distributed synchronization, and providing group services.
+
+This package follows the [offregister](https://github.com/offscale/offregister) specification to setup and serve ZooKeeper.
+
+## Install dependencies
+
+    pip install -r requirements.txt
+
+## Install package
+
+    pip install .
---
+++ offregister_zookeeper/ubuntu.py
@@ -0,0 +1,26 @@
+# -*- coding: utf8 -*-
+
+from fabric.api import run, sudo, local
+
+from offregister_fab_utils.apt import apt_depends, Package
+
+
+def install(*args, **kwargs):
+    version = '3.4.5'
+    apt_depends(Package(name='zookeeper', version=version),
+                Package(name='zookeeperd', version=version))
+
+    # /etc/zookeeper/conf/zoo.cfg
+    '''$ tree /etc/zookeeper/conf
+       ├── configuration.xsl
+       ├── environment
+       ├── log4j.properties
+       ├── myid
+       └── zoo.cfg'''
+
+    return 'zookeeper installed'
+
+
+def serve(*args, **kwargs):
+    if run('service zookeeper status') == 'zookeeper stop/waiting':
+        sudo('service zookeeper start')
