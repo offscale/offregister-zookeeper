6d82f31683e41f1627c4ad84920072ae6e95f8a5
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-15 15:11:03 +1000

    [README.md] New badges; [LICENSE-{APACHE,MIT}] Update copyright year range; [*.py] Python 3 support; Blacken; [requirements.txt,setup.py] Fix dependencies to reflect new names / Python 3

--- offregister_zookeeper/ubuntu.py
+++ offregister_zookeeper/ubuntu.py
@@ -6,21 +6,23 @@ from offregister_fab_utils.apt import apt_depends, Package
 
 
 def install(*args, **kwargs):
-    version = '3.4.5'
-    apt_depends(Package(name='zookeeper', version=version),
-                Package(name='zookeeperd', version=version))
+    version = "3.4.5"
+    apt_depends(
+        Package(name="zookeeper", version=version),
+        Package(name="zookeeperd", version=version),
+    )
 
     # /etc/zookeeper/conf/zoo.cfg
-    '''$ tree /etc/zookeeper/conf
+    """$ tree /etc/zookeeper/conf
        ├── configuration.xsl
        ├── environment
        ├── log4j.properties
        ├── myid
-       └── zoo.cfg'''
+       └── zoo.cfg"""
 
-    return 'zookeeper installed'
+    return "zookeeper installed"
 
 
 def serve(*args, **kwargs):
-    if run('service zookeeper status') == 'zookeeper stop/waiting':
-        sudo('service zookeeper start')
+    if run("service zookeeper status") == "zookeeper stop/waiting":
+        sudo("service zookeeper start")
--- setup.py
+++ setup.py
@@ -1,45 +1,50 @@
 from setuptools import setup, find_packages
 from os import path, listdir
 from functools import partial
-from itertools import imap, ifilter
 from ast import parse
 from distutils.sysconfig import get_python_lib
 
-if __name__ == '__main__':
-    package_name = 'offregister_zookeeper'
+if __name__ == "__main__":
+    package_name = "offregister_zookeeper"
 
     f_for = partial(path.join, path.dirname(__file__), package_name)
     d_for = partial(path.join, get_python_lib(), package_name)
-    to_funcs = lambda name: (partial(path.join, f_for(name)), partial(path.join, d_for(name)))
+    to_funcs = lambda name: (
+        partial(path.join, f_for(name)),
+        partial(path.join, d_for(name)),
+    )
 
-    _data_join, _data_install_dir = to_funcs('data')
+    _data_join, _data_install_dir = to_funcs("data")
 
-    get_vals = lambda var0, var1: imap(lambda buf: next(imap(lambda e: e.value.s, parse(buf).body)),
-                                       ifilter(lambda line: line.startswith(var0) or line.startswith(var1), f))
+    get_vals = lambda var0, var1: list(
+        map(
+            lambda buf: next([e.value.s for e in parse(buf).body]),
+            [line for line in f if line.startswith(var0) or line.startswith(var1)],
+        )
+    )
 
-    with open(path.join(package_name, '__init__.py')) as f:
-        __author__, __version__ = get_vals('__version__', '__author__')
+    with open(path.join(package_name, "__init__.py")) as f:
+        __author__, __version__ = get_vals("__version__", "__author__")
 
     setup(
         name=package_name,
         author=__author__,
         version=__version__,
-        description='Apache ZooKeeper deployment module for Fabric (offregister)',
+        description="Apache ZooKeeper deployment module for Fabric (offregister)",
         classifiers=[
-            'Development Status :: 7 - Inactive',
-            'Intended Audience :: Developers',
-            'Topic :: Software Development',
-            'Topic :: Software Development :: Libraries :: Python Modules',
-            'License :: OSI Approved :: MIT License',
-            'License :: OSI Approved :: Apache Software License',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 2 :: Only'
+            "Development Status :: 7 - Inactive",
+            "Intended Audience :: Developers",
+            "Topic :: Software Development",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "License :: OSI Approved :: MIT License",
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.7",
         ],
-        test_suite=package_name + '.tests',
+        test_suite=package_name + ".tests",
         packages=find_packages(),
         package_dir={package_name: package_name},
         data_files=[
-            (_data_install_dir(), map(_data_join, listdir(_data_join()))),
-        ]
+            (_data_install_dir(), list(map(_data_join, listdir(_data_join())))),
+        ],
     )
